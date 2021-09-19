02cd8b2c28b9f7e8de164a85725ca487f21e919e
Joe Shimamoto <joseph.shimamoto@gmail.com>
2021-09-19 16:27:31 -0700

    added maxpoints to groupview

--- Hearts/urls.py
+++ Hearts/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name = 'register'),
     path('', include('home_app.urls')),
     path('', include("django.contrib.auth.urls")),
-]
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
+    ]
