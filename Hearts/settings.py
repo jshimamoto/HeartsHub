7b41b3eb546080e427081f9a53ae761d9d3ecddd
Joe Shimamoto <joseph.shimamoto@gmail.com>
2022-10-15 18:17:11 -0700

    environ vars

--- Hearts/settings.py
+++ Hearts/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2@h)fxky+iy_b=&c!s3=$wd7qjd9@a^sm$r$@^se#qs78%)cy'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hearts-alpha.herokuapp.com']
 #'heartshub.herokuapp.com'
 
 # Application definition
