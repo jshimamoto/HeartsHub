36096c337752d067c165a197d1d62925e3d2a99e
Joe Shimamoto <joseph.shimamoto@gmail.com>
2022-10-15 18:27:04 -0700

    dependencies

--- Hearts/settings.py
+++ Hearts/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 #import django_heroku
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,4 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CRISPY_TEMPLATE_PACK='bootstrap4'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
